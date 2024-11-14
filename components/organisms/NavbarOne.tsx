10bbddbc37abe0179246ac63f7f828598802bb02
flowkeyz1996 <jonielsanni@gmail.com>
2024-11-15 00:45:00 +0100

    flex

--- components/organisms/NavbarOne.tsx
+++ components/organisms/NavbarOne.tsx
@@ -112,16 +112,16 @@ const NavbarOne: React.FC = () => {
             <Image src="/features-nav-img.svg" alt="Features Icon" width={20} height={20} className="mr-3" />
             <span className="flex-1 text-gray-600">Features</span>
           </Link>
-          <hr className="w-full border-gray-200" />
+          {/* <hr className="w-full border-gray-200" />
           <Link href="/benefit" className="flex items-center px-4 py-2 text-gray-700 w-full hover:bg-gray-100">
             <Image src="/benefit-nav-img.svg" alt="Benefits Icon" width={20} height={20} className="mr-3" />
             <span className="flex-1 text-gray-600">Benefits</span>
-          </Link>
+          </Link> */}
           <hr className="w-full border-gray-200" />
-          <Link href="/company" className="flex items-center px-4 py-2 text-gray-700 w-full hover:bg-gray-100">
+          {/* <Link href="/company" className="flex items-center px-4 py-2 text-gray-700 w-full hover:bg-gray-100">
             <Image src="/company-nav-img.svg" alt="Company Icon" width={20} height={20} className="mr-3" />
             <span className="flex-1 text-gray-600">Company</span>
-          </Link>
+          </Link> */}
           <hr className="w-full border-gray-200" />
           <Link href="/contact" className="flex items-center px-4 py-2 text-gray-700 w-full hover:bg-gray-100">
             <Image src="/blog-nav-img.svg" alt="Blog Icon" width={20} height={20} className="mr-3" />
