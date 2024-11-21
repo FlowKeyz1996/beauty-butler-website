a6a3586b6825bb607300ae4ef14aa1d613c23421
flowkeyz1996 <jonielsanni@gmail.com>
2024-11-21 09:59:41 +0100

    download app

--- components/organisms/HowItWork.tsx
+++ components/organisms/HowItWork.tsx
@@ -20,11 +20,11 @@ const HowItWork: React.FC = () => {
   return (
     <div
       ref={sectionRef}
-      className="relative flex flex-col items-center justify-center bg-[#8877d8] h-screen w-screen"
+      className="relative flex flex-col items-center justify-center bg-[#8877d8] h-screen w-screen px-4 sm:px-8"
     >
       {/* Animate the heading coming from the top when in view */}
       {/* <motion.h2
-        className="absolute top-10 text-3xl sm:text-4xl font-apfelmittel mb-5 sm:mb-7 text-white z-10"
+        className="absolute top-5 sm:top-10 text-2xl sm:text-4xl font-apfelmittel mb-5 sm:mb-7 text-white z-10"
         initial={{ y: -100, opacity: 0 }}
         animate={isInView ? { y: 0, opacity: 1 } : {}}
         transition={{ duration: 2, ease: 'easeOut' }}
@@ -34,14 +34,14 @@ const HowItWork: React.FC = () => {
 
       {/* Animate the video sliding in from the left when in view */}
       <motion.div
-        className="relative w-full h-full"
+        className="relative w-full h-[60vh] sm:h-[75vh] md:h-full"
         initial={{ x: '-100vw', opacity: 0 }}
         animate={isInView ? { x: 0, opacity: 1 } : {}}
         transition={{ type: 'spring', duration: 2 }}
       >
         <video
           id="howItWorksVideo"
-          className="absolute top-0 left-0 w-full h-full object-cover"
+          className="absolute top-0 left-0 w-full h-full object-cover rounded-md"
           controls={isPlaying}
           poster="/cover-img-for-video.png"
         >
@@ -52,7 +52,7 @@ const HowItWork: React.FC = () => {
         {!isPlaying && (
           <button
             onClick={handlePlay}
-            className="absolute inset-0 flex items-center justify-center text-white text-5xl sm:text-6xl z-10 border-3"
+            className="absolute inset-0 flex items-center justify-center text-white text-4xl sm:text-5xl md:text-6xl z-10 bg-opacity-70"
           >
             &#9654; {/* Play button */}
           </button>
@@ -61,7 +61,7 @@ const HowItWork: React.FC = () => {
 
       {/* <Link href="https://airtable.com/appbyIQgO429LydxX/pagpJUqsZCrQzQcYg/form">
         <motion.button
-          className="absolute bottom-10 bg-white text-[#8877d8] py-3 px-10 sm:py-4 sm:px-24 rounded-2xl sm:text-2xl text-xl font-euclidmedium z-10"
+          className="absolute bottom-5 sm:bottom-10 bg-white text-[#8877d8] py-2 px-6 sm:py-3 sm:px-10 rounded-lg sm:rounded-2xl text-lg sm:text-xl md:text-2xl font-euclidmedium z-10"
           initial={{ scale: 0 }}
           animate={isInView ? { scale: 1 } : {}}
           transition={{ duration: 1, ease: 'easeOut' }}
